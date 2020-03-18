2917fcfb9e682dfd06f39b48706ca950694a1160
louis.genestier <louis.genestier@gmail.com>
2020-03-18 19:23:40 +0100

    using curl instead of HttpClient

--- src/Service/EtablissementPublicApi.php
+++ src/Service/EtablissementPublicApi.php
@@ -3,21 +3,16 @@
 
 namespace App\Service;
 
-
-use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class EtablissementPublicApi
 {
 
     private $code;
-    /** * @var \Symfony\Contracts\HttpClient\HttpClientInterface */
-    private $client;
 
     public function __construct(String $code)
     {
         $this->code = $code;
-        $this->client = HttpClient::create();
     }
 
     public function getEtablissements()
@@ -25,8 +20,11 @@ class EtablissementPublicApi
         $url = "https://etablissements-publics.api.gouv.fr/v3/communes/$this->code/pole_emploi";
 
         try {
-            $response = $this->client->request('GET', $url);
-            $data = \json_decode($response->getContent(), true)['features'];
+            $ch = \curl_init();
+            \curl_setopt($ch, CURLOPT_URL, $url);
+            \curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $response = \curl_exec($ch);
+            $data = \json_decode($response, true)['features'];
             return $data;
         } catch (TransportExceptionInterface $e) {
             dump($e->getMessage());
--- src/Service/GeoApi.php
+++ src/Service/GeoApi.php
@@ -3,8 +3,6 @@
 
 namespace App\Service;
 
-
-use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class GeoApi
@@ -12,25 +10,25 @@ class GeoApi
 
     private $zip;
     private $city;
-    /** * @var \Symfony\Contracts\HttpClient\HttpClientInterface */
-    private $client;
 
 
     public function __construct(String $city, String $zip)
     {
         $this->city = $city;
         $this->zip = $zip;
-        $this->client = HttpClient::create();
     }
 
     public function getCityCode()
     {
         $url = "https://geo.api.gouv.fr/communes?codePostal=$this->zip&nom=$this->city&fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre";
         try {
-            $response = $this->client->request('GET', $url);
-            $data = \json_decode($response->getContent(), true);
+            $ch = \curl_init();
+            \curl_setopt($ch, CURLOPT_URL, $url);
+            \curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $response = \curl_exec($ch);
+            $data = \json_decode($response, true)[0];
             if (!empty($data)) {
-                return $data[0]['code'];
+                return $data['code'];
             }
             return null;
         } catch (TransportExceptionInterface $e) {
